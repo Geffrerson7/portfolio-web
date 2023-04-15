6a11346526126c81e1fe4c90bd50b9381f6a4a93
Gefferson Casasola <gcasasolah@uni.pe>
2023-04-15 18:23:11 -0500

    Add navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,59 @@
 import Link from 'next/link'
 import React from 'react'
+import Logo from './Logo'
+import { useRouter } from 'next/router'
+import { GitHubIcon, LinkedInIcon, TwitterIcon } from './Icons'
+import { motion } from "framer-motion"
 
+const CustomLink = ({ href, title, className = "" }) => {
+    const router = useRouter();
+    return (
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+            <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
+        </Link>
+    )
+}
 const NavBar = () => {
-  return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
-        <nav>
-            <Link href="/">Home</Link>
-            <Link href="/">About</Link>
-            <Link href="/">Projects</Link>
-        </nav>
-        <h2>Logo</h2>
-        <nav>
-            <Link href="/" target={"_blank"}>Linkedin</Link>
-            <Link href="/" target={"_blank"}>Github</Link>
-            <Link href="/" target={"_blank"}>Twitter</Link>
-        </nav>
-    </header>
-  )
+    return (
+        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+            <nav>
+                <CustomLink href="/" title="Home" className='mr-4' />
+                <CustomLink href="/about" title="About" className='mx-4' />
+                <CustomLink href="/projects" title="Projects" className='ml-4' />
+            </nav>
+
+            <nav className='flex items-center justify-center flex-wrap'>
+                <motion.a href="https://www.linkedin.com/in/gefferson-casasola/" 
+                target={"_blank"}
+                whileHover={{y:-2}}
+                whileTap={{scale:0.9}}
+                className='w-6 mr-3'
+                >
+                    <LinkedInIcon />
+                </motion.a>
+                <motion.a href="https://github.com/Geffrerson7" 
+                target={"_blank"}
+                whileHover={{y:-2}}
+                whileTap={{scale:0.9}}
+                className='w-6 mx-3'
+                >
+                    <GitHubIcon />
+                </motion.a>
+                <motion.a href="https://twitter.com/gcasasolah" 
+                target={"_blank"}
+                whileHover={{y:-2}}
+                whileTap={{scale:0.9}}
+                className='w-6 ml-3'
+                >
+                    <TwitterIcon />
+                </motion.a>
+            </nav>
+            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+                <Logo />
+            </div>
+        </header>
+    )
 }
 
 export default NavBar
