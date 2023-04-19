a0b3b70e59d9d9a0e5b18c38ec9aa4dcf94f1a21
Gefferson Casasola <gcasasolah@uni.pe>
2023-04-19 14:42:03 -0500

    Add responsiveness to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,27 +20,27 @@ export default function Home() {
 
       </Head>
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-        <Layout className='pt-0'>
-          <div className='flex items-center justify-between w-full'>
-            <div className='w-1/2'>
-              <Image src={profilePic} alt="CodeGef" className='w-full h-auto' 
+        <Layout className='pt-0 md:p-16 sm:pt-8'>
+          <div className='flex items-center justify-between w-full lg:flex-col'>
+            <div className='w-1/2 md:w-full'>
+              <Image src={profilePic} alt="CodeGef" className='w-full h-auto lg:hidden md:inline-block md:w-full' 
               priority
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
               />
             </div>
-            <div className='w-1/2 flex flex-col items-center self-center'>
+            <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
 
-              <AnimatedText text='Hi, I&#39;m Gefferson Casasola' className='!text-6xl !text-left' />
-              <p className='my-4 text-base font-medium'>
+              <AnimatedText text='Hi, I&#39;m Gefferson Casasola' className='!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl' />
+              <p className='my-4 text-base font-medium md:text-small sm:text-xs'>
                 I&#39;m a graduate of electronic engineering and I&#39;m currently working as a junior backend developer. Let&#39;s build something together.
               </p>
-              <div className='flex items-center self-start mt-2'>
+              <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="https://rxresu.me/gcasasolah/cvgef" target="_blank"
                   className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
-                   dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light'
+                   dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:pd-2 md:px-4 md:text-base'
                 >Resume <LinkArrow className={"w-6 ml-1"} /></Link>
                 <Link href="mailto:gefferson.casasola@gmail.com" target="_blank"
-                  className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light'
+                  className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'
                 >Contact</Link>
               </div>
             </div>
@@ -48,7 +48,7 @@ export default function Home() {
 
         </Layout>
         <HireMe />
-        <div className='absolute right-8 bottom-8 inline-block w-24'>
+        <div className='absolute right-8 bottom-8 inline-block w-24 md:hidden'>
           <Image src={lightBulb} alt="CodeGef" className='w-full h-auto' />
         </div>
       </main>
