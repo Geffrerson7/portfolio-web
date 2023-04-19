1e96024a63ec0726c5b59e1e2510862da91b41a1
Gefferson Casasola <gcasasolah@uni.pe>
2023-04-19 00:25:16 -0500

    Add burger icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
-import { GitHubIcon, LinkedInIcon, MoonIcon,SunIcon, TwitterIcon } from './Icons'
+import { GitHubIcon, LinkedInIcon, MoonIcon, SunIcon, TwitterIcon } from './Icons'
 import { motion } from "framer-motion"
 import useThemeSwitcher from './hooks/useThemeSwitcher'
+
 
 const CustomLink = ({ href, title, className = "" }) => {
     const router = useRouter();
@@ -18,51 +19,65 @@ const CustomLink = ({ href, title, className = "" }) => {
 const NavBar = () => {
 
     const [mode, setMode] = useThemeSwitcher()
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
 
     return (
-        <header className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light'>
-            <nav>
-                <CustomLink href="/" title="Home" className='mr-4' />
-                <CustomLink href="/about" title="About" className='mx-4' />
-                <CustomLink href="/projects" title="Projects" className='ml-4' />
-            </nav>
+        <header className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light' onClick={handleClick}>
+            <button className='flex-col justify-center items-center hidden lg:flex'>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
+            </button>
 
-            <nav className='flex items-center justify-center flex-wrap'>
-                <motion.a href="https://www.linkedin.com/in/gefferson-casasola/" 
-                target={"_blank"}
-                whileHover={{y:-2}}
-                whileTap={{scale:0.9}}
-                className='w-6 mr-3'
-                >
-                    <LinkedInIcon />
-                </motion.a>
-                <motion.a href="https://github.com/Geffrerson7" 
-                target={"_blank"}
-                whileHover={{y:-2}}
-                whileTap={{scale:0.9}}
-                className='w-6 mx-3'
-                >
-                    <GitHubIcon />
-                </motion.a>
-                <motion.a href="https://twitter.com/gcasasolah" 
-                target={"_blank"}
-                whileHover={{y:-2}}
-                whileTap={{scale:0.9}}
-                className='w-6 ml-3'
-                >
-                    <TwitterIcon />
-                </motion.a>
+            <div className='w-full flex justify-between items-center lg:hidden'>
+                <nav>
+                    <CustomLink href="/" title="Home" className='mr-4' />
+                    <CustomLink href="/about" title="About" className='mx-4' />
+                    <CustomLink href="/projects" title="Projects" className='ml-4' />
+                </nav>
 
-                <button onClick={() => setMode(mode === "light" ? "dark" : "light")}
-                className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light"? "bg-dark text-light" : "bg-light text-dark"}`}
-                >
-                    {
-                        mode === "dark" ?
-                        <SunIcon className={"fill-dark"}/>
-                        : <MoonIcon className={"fill-dark"}/>
-                    }
-                </button>
-            </nav>
+                <nav className='flex items-center justify-center flex-wrap'>
+                    <motion.a href="https://www.linkedin.com/in/gefferson-casasola/"
+                        target={"_blank"}
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 mr-3'
+                    >
+                        <LinkedInIcon />
+                    </motion.a>
+                    <motion.a href="https://github.com/Geffrerson7"
+                        target={"_blank"}
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 mx-3'
+                    >
+                        <GitHubIcon />
+                    </motion.a>
+                    <motion.a href="https://twitter.com/gcasasolah"
+                        target={"_blank"}
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 ml-3'
+                    >
+                        <TwitterIcon />
+                    </motion.a>
+
+                    <button onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                        className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
+                    >
+                        {
+                            mode === "dark" ?
+                                <SunIcon className={"fill-dark"} />
+                                : <MoonIcon className={"fill-dark"} />
+                        }
+                    </button>
+                </nav>
+            </div>
+            
             <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
                 <Logo />
             </div>
