452239edc776be1a5e587769170228d9c7f38bbb
Gefferson Casasola <gcasasolah@uni.pe>
2023-04-20 16:59:10 -0500

    Refactor files extension

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 const useThemeSwitcher = () => {
     const preferDarkQuery = "(prefer-color-scheme: dark)";
