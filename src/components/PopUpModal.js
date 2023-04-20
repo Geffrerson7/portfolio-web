26831f15038189b72661c7156b4dfc4a7bfb43b5
Gefferson Casasola <gcasasolah@uni.pe>
2023-04-19 20:52:15 -0500

    Add responsiveness to the visit project page

---
+++ src/components/PopUpModal.js
@@ -0,0 +1,34 @@
+
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { AiFillFire } from 'react-icons/ai'
+
+
+const PopUpModal = ({ srcImg, description }) => {
+    const [showModal, setShowModal] = useState(false)
+    return (
+        <div className='flex md:flex-col justify-center items-center mt-5'>
+
+            {showModal ?
+                (
+                    <div className='mt-10 flex justify-center items-center flex-col w-auto rounded-lg shadow-xl h-auto p-2 dark:bg-light md:w-[50%] md:h-[50%] lg:w-[75%] lg:h-[75%]'>
+                        <Image src={srcImg} />
+                        <h2 className='text-base mt-2 mx-4 text-primary font semibold text-center md:text-small sm:text-xs md:text-center sm:text-center'>{description}</h2>
+                        <button className='my-5 w-auto px-8 h-10 bg-blue-600 text-white rounded-md shadow hover:shadow-lg font-semibold md:h-5 md:px-4 md:text-small sm:text-xs md:text-center sm:text-center'
+                            onClick={() => setShowModal(false)}>Close</button>
+                    </div>
+                ) :
+                (
+                    <div className='flex gap-5'>
+                        <button type="button" className='bg-primary dark:bg-primaryDark text-light dark:text-dark active:bg-black dark:active:bg-light hover:bg-black dark:hover:bg-light flex justify-center items-center gap-2 font-bold px-6 h-12 rounded-md shadow hover:shadow-lg outline-none focus:outline-none md:h-6 md:px-3 md:text-small sm:text-xs md:text-center sm:text-center'
+                            onClick={() => setShowModal(true)}
+                        >View Image <AiFillFire className='text-xl' />
+                        </button>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default PopUpModal
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -6,12 +6,12 @@ import { motion } from 'framer-motion';
 const Technologies = ({name, version, url, description}) => {
     const ref = useRef(null);
   return (
-    <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+    <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]'>
         <LiIcon reference={ref}/>
         <motion.div initial={{y:50}} whileInView={{y:0}} transition={{duration:0.5, type:"spring"}}>
-            <h3 className='capitalize font-bold text-2xl'>{name}&nbsp;<a href={url} target='_blank' className='text-primary dark:text-primaryDark capitalize'>&#40;Source&#41;</a></h3>
-            <span className='capitalize font-medium text-dark/75 dark:text-light/75'>Version: {version}</span>
-            <p className='font-medium w-full'>{description}</p>
+            <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>{name}&nbsp;<a href={url} target='_blank' className='text-primary dark:text-primaryDark capitalize'>&#40;Source&#41;</a></h3>
+            <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>Version: {version}</span>
+            <p className='font-medium w-full md:text-sm'>{description}</p>
         </motion.div>
     </li>
   )
