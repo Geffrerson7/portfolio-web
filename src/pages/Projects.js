dfe5ade36332f648b65fdf41e0823f96b66d8c28
Gefferson Casasola <gcasasolah@uni.pe>
2024-03-31 14:34:49 -0500

    Add projects selector color

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,221 +5,11 @@ import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import Image from "next/image";
 import { GitHubIcon } from "@/components/Icons";
-import airlineImg from "../../public/images/projects/airline-check-in.png";
-import servicePaymentsImg from "../../public/images/projects/service-payments-api.png";
-import playlistsImg from "../../public/images/projects/playlists-api.png";
-import moviesImg from "../../public/images/projects/movies-api.png";
-import portfolioDjangoImg from "../../public/images/projects/django-portfolio.png";
-import inventorySystemImg from "../../public/images/projects/inventory-sys.png";
-import rickAndMortyImg from "../../public/images/projects/rick-and-morty-app.png";
-import lambdaFunctionImg from "../../public/images/projects/lambda-function.png";
 import { motion } from "framer-motion";
 import TransitionEffect from "@/components/TransitionEffect";
 import { useState } from "react";
-import servicesPaymentsFrontendImg from "../../public/images/projects/screenpayments-1.png";
-import jobsImg from "../../public/images/projects/jobs-api.png";
-import jobsapifrontendImg from "../../public/images/projects/jobs-app-1.png";
-import jobsCrudFlaskImg from "../../public/images/projects/job-crud-flask.png";
-import bookStorageAndPokepediaImg from "../../public/images/projects/pokepedia.png";
-import hotelBookingApiImg from "../../public/images/projects/hotel-booking-api.png"
-import onlineCoursesApiImg from "../../public/images/projects/online-courses-api.png"
-import onlineCoursesApiFrontendImg from "../../public/images/projects/online-courses-api-frontend-1.png"
-import projectsApiImg from "../../public/images/projects/projects-api.png"
-import IV100TelegramBotImg from "../../public/images/projects/iv-100.png"
-
+import projectsData from "../data/projects.js";
 const FramerImage = motion(Image);
-
-const projectsData = [
-  {
-    id: 1,
-    type: "Django",
-    title: "Airline Check-in",
-    summary:
-      "It is a REST API with a single endpoint that allows querying by flight ID and returns the simulation of an automatic check-in for passengers of Andes Airlines.",
-    img: airlineImg,
-    link: "/AirlineCheckIn",
-    github: "https://github.com/Geffrerson7/airline-check-in",
-  },
-  {
-    id: 2,
-    type: "Django",
-    title: "Service Payments API",
-    summary:
-      "Streaming service payment API. It has the functionalities of creating payments and automatically registering if it is expired, creating and updating streaming services, and creating a user with a profile picture.",
-    img: servicePaymentsImg,
-    link: "/ServicesPaymentsAPI",
-    github: "https://github.com/Geffrerson7/DJANGO-PAYMENTS-API",
-  },
-  {
-    id: 3,
-    type: "Node",
-    title: "Playlists API",
-    img: playlistsImg,
-    summary:
-      "It is an API that creates, lists and updates song playlists and songs in public or private mode for authenticated users. It also has the functionality to create and list users and login. This app was built using TypeScript, Express, Prisma and PostgreSQL.For authentication users we used JWT and for API documentation I used Postman.",
-    link: "/PlaylistAPI",
-    github: "https://github.com/Geffrerson7/PLAYLIST-API",
-  },
-  {
-    id: 4,
-    type: "Node",
-    title: "Movies API",
-    summary:
-      "This app was built using TypeScript, Express, Prisma and PostgreSQL. You are able to create users, clients, movies and movies copy. For authentication users I used JWT and for API documentation I used Postman.",
-    img: moviesImg,
-    link: "/MoviesAPI",
-    github: "https://github.com/Geffrerson7/APP-MOVIES",
-  },
-  {
-    id: 5,
-    type: "Flask",
-    title: "RICK AND MORTY APP",
-    summary:
-      "This is an application that displays data on the characters from the Rick and Morty series, including pagination, and also shows data on characters that appear in a specific episode. It's designed using Flask, Python, MongoDB, HTML, and Bootstrap.",
-    img: rickAndMortyImg,
-    link: "/RickAndMorty",
-    github: "https://github.com/Geffrerson7/FLASK-APP-RICK-AND-MORTY",
-  },
-  {
-    id: 6,
-    type: "Node",
-    title: "System Inventory",
-    summary:
-      "This app was built using TypeScript, Express, Prisma, Pusher, Twilio, Supabase and PostgreSQL for the backend and Angular and Typescript por the frontend, You are able to create users, clients, suppliers, products, categories and orders. Also authenticated users will have access to private songs. For authentication users we used JWT.",
-    img: inventorySystemImg,
-    link: "/InventorySystem",
-    github: "https://github.com/Geffrerson7/Proyecto-Unidad-8-Back",
-  },
-  {
-    id: 7,
-    type: "Django",
-    title: "Django Portfolio",
-    summary:
-      "It is a portfolio where you can view and add your programming projects. The Back End part was done with Django and the Front End part was done with Bootstrap and a free template.",
-    img: portfolioDjangoImg,
-    link: "/DjangoPortfolio",
-    github: "https://github.com/Geffrerson7/PORTFOLIO-DJANGO",
-  },
-  {
-    id: 8,
-    type: "Python",
-    title: "Mercadopago Lambda function",
-    summary:
-      "Lambda function for an endpoint that creates a payment with the MercadoPago library in Python, using AWS Lambda and API Gateway services.",
-    img: lambdaFunctionImg,
-    link: "/LambdaFunction",
-    github: "https://github.com/Geffrerson7/Lambda-function-U6",
-  },
-  {
-    id: 9,
-    type: "React",
-    title: "Services Payments Api Frontend",
-    summary:
-      "Application to create service payments, create and update services, and classify payments as paid or overdue. It was designed with HTML, CSS, JavaScript, React, Vite and TailwindCSS.",
-    img: servicesPaymentsFrontendImg,
-    link: "/ServicesPaymentsApiFrontend",
-    github: "https://github.com/Geffrerson7/SERVICES-PAYMENTS-API-FRONTEND",
-  },
-  {
-    id: 10,
-    type: "Django",
-    title: "Jobs Api",
-    summary: "API to monitor your job applications on different web portals.",
-    img: jobsImg,
-    link: "/JobsAPI",
-    github: "https://github.com/Geffrerson7/JOBS-API",
-  },
-  {
-    id: 11,
-    type: "React",
-    title: "Jobs Api Frontend",
-    summary:
-      "It is an application to list, register, update, and delete your job applications.",
-    img: jobsapifrontendImg,
-    link: "/JobsApiFrontend",
-    github: "https://github.com/Geffrerson7/JOBS-API-FRONTEND",
-  },
-  {
-    id: 12,
-    type: "Flask",
-    title: "Job CRUD",
-    summary:
-      "It is an app built with Flask, Python, HTML, and JavaScript to create, list, update, and delete your job applications.",
-    img: jobsCrudFlaskImg,
-    link: "/JobCRUD",
-    github: "https://github.com/Geffrerson7/JOB-CRUD",
-  },
-  {
-    id: 13,
-    type: "Python",
-    title: "Book Storage and Pokepedia",
-    summary:
-        "It is an application that has two parts: The Book Storage, which is a program that allows you to register, search, and download book data such as title, genre, ISBN, publisher, and author(s) through the command line. And the Pokepedia, which is a program that allows you to search for data from the PokeAPI, such as name, ability, and image URL of the pokemons through the command line.",
-    img: bookStorageAndPokepediaImg,
-    link: "/BookStorageAndPokepedia",
-    github: "https://github.com/Geffrerson7/BOOK-STORAGE-AND-POKEPEDIA",
-  },
-  {
-    id: 14,
-    type: "Node",
-    title: "Airline Check-in Node Version",
-    summary:
-        "It is a REST API with a single endpoint that allows querying by flight ID and returns the simulation of an automatic check-in for passengers of Andes Airlines.",
-    img: airlineImg,
-    link: "/AirlineCheckInNodeVersion",
-    github: "https://github.com/Geffrerson7/simulacion-check-in-aerolinea",
-  },
-  {
-    id: 15,
-    type: "Django",
-    title: "Hotel Booking API",
-    summary:
-        "It is an API for making online reservations in a hotel.",
-    img: hotelBookingApiImg,
-    link: "/HotelBookingAPI",
-    github: "https://github.com/Geffrerson7/HOTEL-RESERVATION-API",
-  },
-  {
-    id: 16,
-    type: "Django",
-    title: "Online Courses API",
-    summary:
-        "It is an API for simulating online course purchases using the Mercado Pago API.",
-    img: onlineCoursesApiImg,
-    link: "/OnlineCoursesAPI",
-    github: "https://github.com/Geffrerson7/ONLINE-COURSES-API",
-  },
-  {
-    id: 17,
-    type: "React",
-    title: "Online Courses API Frontend",
-    summary:
-        "It is an application to make online course purchases.",
-    img: onlineCoursesApiFrontendImg,
-    link: "/OnlineCoursesApiFrontend",
-    github: "https://github.com/Geffrerson7/ONLINE-COURSES-API-FRONTEND",
-  },
-  {
-    id: 18,
-    type: "Flask",
-    title: "Projects API",
-    summary:
-        "It is an application to make online course purchases.",
-    img: projectsApiImg,
-    link: "/ProjectsAPI",
-    github: "https://github.com/Geffrerson7/PROJECTS-API",
-  },
-  {
-    id: 19,
-    type: "Python",
-    title: "IV 100 Telegram Bot",
-    summary:
-        "Telegram bot for sending Pokemon coordinates with IV 100.",
-    img: IV100TelegramBotImg,
-    link: "/IV100TelegramBot",
-    github: "https://github.com/Geffrerson7/iv100-bot",
-  },
-];
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
@@ -326,10 +116,22 @@ const Project = ({ type, title, summary, img, link, github }) => {
 };
 
 const Projects = () => {
+  const [showAll, setShowAll] = useState(true);
+  const [showReact, setShowReact] = useState(false);
+  const [showNode, setShowNode] = useState(false);
+  const [showDjango, setShowDjango] = useState(false);
+  const [showFlask, setShowFlask] = useState(false);
+  const [showPython, setShowPython] = useState(false);
   const [projectsToShow, setProjectsToShow] = useState(projectsData);
 
   const handleShowAllProjects = () => {
     setProjectsToShow(projectsData);
+    setShowAll(true);
+    setShowReact(false);
+    setShowNode(false);
+    setShowDjango(false);
+    setShowFlask(false);
+    setShowPython(false);
   };
 
   const handleShowDjangoProjects = () => {
@@ -337,6 +139,12 @@ const Projects = () => {
       (project) => project.type === "Django"
     );
     setProjectsToShow(DjangoProjects);
+    setShowAll(false);
+    setShowReact(false);
+    setShowNode(false);
+    setShowDjango(true);
+    setShowFlask(false);
+    setShowPython(false);
   };
 
   const handleShowNodeProjects = () => {
@@ -344,6 +152,12 @@ const Projects = () => {
       (project) => project.type === "Node"
     );
     setProjectsToShow(nodeProjects);
+    setShowAll(false);
+    setShowReact(false);
+    setShowNode(true);
+    setShowDjango(false);
+    setShowFlask(false);
+    setShowPython(false);
   };
 
   const handleShowFlaskProjects = () => {
@@ -351,6 +165,12 @@ const Projects = () => {
       (project) => project.type === "Flask"
     );
     setProjectsToShow(flaskProjects);
+    setShowAll(false);
+    setShowReact(false);
+    setShowNode(false);
+    setShowDjango(false);
+    setShowFlask(true);
+    setShowPython(false);
   };
 
   const handleShowPythonProjects = () => {
@@ -358,6 +178,12 @@ const Projects = () => {
       (project) => project.type === "Python"
     );
     setProjectsToShow(pythonProjects);
+    setShowAll(false);
+    setShowReact(false);
+    setShowNode(false);
+    setShowDjango(false);
+    setShowFlask(false);
+    setShowPython(true);
   };
 
   const handleShowReactProjects = () => {
@@ -365,6 +191,12 @@ const Projects = () => {
       (project) => project.type === "React"
     );
     setProjectsToShow(reactProjects);
+    setShowAll(false);
+    setShowReact(true);
+    setShowNode(false);
+    setShowDjango(false);
+    setShowFlask(false);
+    setShowPython(false);
   };
 
   return (
@@ -383,37 +215,61 @@ const Projects = () => {
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="flex justify-center mb-4 mt-4 col-span-12">
               <button
-                className="bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 rounded-l dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800"
+                className={`bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 rounded-l dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800 ${
+                  showAll
+                    ? "bg-primary dark:bg-primaryDark"
+                    : "bg-gray-800 dark:bg-gray-300"
+                }`}
                 onClick={handleShowAllProjects}
               >
                 All
               </button>
               <button
-                className="bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800"
+                className={`bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800 ${
+                  showReact
+                    ? "bg-primary dark:bg-primaryDark"
+                    : "bg-gray-800 dark:bg-gray-300"
+                }`}
                 onClick={handleShowReactProjects}
               >
                 React
               </button>
               <button
-                className="bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800"
+                className={`bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800 ${
+                  showNode
+                    ? "bg-primary dark:bg-primaryDark"
+                    : "bg-gray-800 dark:bg-gray-300"
+                }`}
                 onClick={handleShowNodeProjects}
               >
                 Node
               </button>
               <button
-                className="bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800"
+                className={`bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800 ${
+                  showDjango
+                    ? "bg-primary dark:bg-primaryDark"
+                    : "bg-gray-800 dark:bg-gray-300"
+                }`}
                 onClick={handleShowDjangoProjects}
               >
                 Django
               </button>
               <button
-                className="bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800"
+                className={`bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800 ${
+                  showFlask
+                    ? "bg-primary dark:bg-primaryDark"
+                    : "bg-gray-800 dark:bg-gray-300"
+                }`}
                 onClick={handleShowFlaskProjects}
               >
                 Flask
               </button>
               <button
-                className="bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 rounded-r dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800"
+                className={`bg-gray-800 hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 rounded-r dark:bg-gray-300 dark:hover:bg-gray-400 dark:text-gray-800 ${
+                  showPython
+                    ? "bg-primary dark:bg-primaryDark"
+                    : "bg-gray-800 dark:bg-gray-300"
+                }`}
                 onClick={handleShowPythonProjects}
               >
                 Python
