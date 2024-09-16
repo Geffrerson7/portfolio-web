88c898b4b7e6f686295061232b46a93c8830d147
Gefferson Casasola <gcasasolah@uni.pe>
2024-09-15 20:11:28 -0500

    Fix video project

--- src/components/projects/IV100TelegramBot/IV100TelegramBotVideo.js
+++ src/components/projects/IV100TelegramBot/IV100TelegramBotVideo.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { MdOndemandVideo } from "react-icons/md";
-import Video from "next-video";
-import NewDemoIV100Bot from "../../../../videos/New-Demo-IV-100-Bot.mp4";
 import Link from "next/link";
 
 const IV100TelegramBotVideo = () => {
@@ -10,13 +8,9 @@ const IV100TelegramBotVideo = () => {
       <h3 className="font-bold text-4xl mb-16 mt-8 w-full text-center md:text-3xl xs:text-2xl md:mb-8 dark:text-light">
         VIDEO DEMO
       </h3>
-      <Video
-        src={NewDemoIV100Bot}
-        style={{ maxWidth: "42rem", width: "100%" }}
-      />
       <div className="flex flex-row items-center mt-4 mb-16 text-2xl xs:text-xs md:text-base">
         <MdOndemandVideo className="dark:text-light" />
-        <Link href={`https://youtu.be/Yqj4HdKhS-s`} target="_blank">
+        <Link href={`https://youtube.com/shorts/bGI4qAefg_c`} target="_blank">
           <h3 className="dark:text-light font-bold ml-4 hover:underline">
             Click to watch full video
           </h3>
