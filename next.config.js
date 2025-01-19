b01e537d2bd708f3cfe125488e0a7e53ec552a5d
Gefferson Casasola <gcasasolah@uni.pe>
2025-01-19 14:27:38 -0500

    feat: update project setting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const { withNextVideo } = require('next-video/process')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withNextVideo(nextConfig)
+module.exports = nextConfig;
+
